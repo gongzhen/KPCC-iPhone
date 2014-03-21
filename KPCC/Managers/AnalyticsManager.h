ada3b897444d0073efc4dbf2a6727a7d53f50532
John Meeker <jmeeker516@gmail.com>
2014-03-21 14:26:43 -0700

    Create AnalyticsManager and NSDate helper.

---
+++ KPCC/Managers/AnalyticsManager.h
@@ -0,0 +1,13 @@
+//
+//  AnalyticsManager.h
+//  KPCC
+//
+//  Created by John Meeker on 3/21/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnalyticsManager : NSObject
+
+@end
---
+++ KPCC/Managers/AnalyticsManager.m
@@ -0,0 +1,13 @@
+//
+//  AnalyticsManager.m
+//  KPCC
+//
+//  Created by John Meeker on 3/21/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import "AnalyticsManager.h"
+
+@implementation AnalyticsManager
+
+@end
