ca9be3e0f7ee54604a6c4ed87a72eef4e0f26df5
John Meeker <jmeeker516@gmail.com>
2014-04-02 09:47:28 -0700

    Begin contructing view programmatically and add to scrollview.

--- KPCC/UI/Controllers/SCPRRootViewController.m
+++ KPCC/UI/Controllers/SCPRRootViewController.m
@@ -8,25 +8,63 @@
 
 #import "SCPRRootViewController.h"
 
-@interface SCPRRootViewController ()
-
+@interface SCPRRootViewController () <UIScrollViewDelegate>
+@property (nonatomic) UILabel *onAirLabel;
+@property (nonatomic) UILabel *programTitleLabel;
 @end
 
 @implementation SCPRRootViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+#pragma mark - Accessors
+
+@synthesize onAirLabel = _onAirLabel;
+@synthesize programTitleLabel = _programTitleLabel;
+
+- (UILabel *)onAirLabel {
+    if (!_onAirLabel) {
+        _onAirLabel = [[UILabel alloc] init];
+        _onAirLabel.textColor = [UIColor darkGrayColor];
+        _onAirLabel.text = @"On air now:";
+        //[_onAirLabel sizeToFit];
     }
-    return self;
+    return _onAirLabel;
 }
+
+- (UILabel *)programTitleLabel {
+    if (!_programTitleLabel) {
+        _programTitleLabel = [[UILabel alloc] init];
+        _programTitleLabel.textColor = [UIColor colorWithRed:71.0f/255.0f green:111.0f/255.0f blue:192.0f/255.0f alpha:1.0f];
+        _programTitleLabel.text = @"All Things Considered";
+        //[_programTitleLabel sizeToFit];
+    }
+    return _programTitleLabel;
+}
+
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.navigationItem.title = @"KPCC";
+    
+    UIScrollView *scrollview = [[UIScrollView alloc] initWithFrame:self.view.frame];
+
+    [scrollview addSubview:self.onAirLabel];
+    [scrollview addSubview:self.programTitleLabel];
+    
+    [scrollview setContentSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height - 60)];
+    [self.view addSubview:scrollview];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+
+    CGSize size = self.view.bounds.size;
+    
+    self.onAirLabel.frame = CGRectMake(20.0f, 20.0f, 90.0f, 20.f);
+    self.programTitleLabel.frame = CGRectMake(120.f, 20.0f, size.width - 120.0f, 20.0f);
 }
 
 - (void)didReceiveMemoryWarning
