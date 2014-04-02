7b6084267c09e8de027c7e9d3a0a5a3549e20955
John Meeker <jmeeker516@gmail.com>
2014-04-02 09:09:12 -0700

    Update project settings and application-wide styling.

--- KPCC/SCPRAppDelegate.m
+++ KPCC/SCPRAppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "SCPRAppDelegate.h"
+#import "SCPRHomeViewController.h"
+#import "SCPRRootViewController.h"
 #import <AVFoundation/AVFoundation.h>
 #import "Flurry.h"
 
@@ -17,8 +19,7 @@
 
 @implementation SCPRAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSError* error;
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&error];
     [[AVAudioSession sharedInstance] setActive:YES error:&error];
@@ -35,35 +36,49 @@
     [Flurry startSession: [[globalConfig objectForKey:@"Flurry"] objectForKey:@"DebugKey"] ];
     [Flurry setBackgroundSessionEnabled:NO];
     
+    // Apply application-wide styling
+    [self applyStylesheet];
+    
+    // Initialize the window
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    // Launch our root view controller
+    SCPRRootViewController *viewController = [[SCPRRootViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    self.window.rootViewController = navigationController;
+    
     // Override point for customization after application launch.
     return YES;
 }
 							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+# pragma mark - Stylesheet
+
+- (void)applyStylesheet {
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:248.0f/255.0f green:106.0f/255.0f blue:39.0f/255.0f alpha:1.0f]];
+    [[UINavigationBar appearance] setBarStyle:UIBarStyleBlackTranslucent];
 }
 
 @end
