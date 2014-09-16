b98c8f35f1345b486a7a331a895fac2a7a33e947
John Meeker <jmeeker516@gmail.com>
2014-09-16 09:56:45 -0700

    Create SCPRProgramDetailViewController and xib.

---
+++ KPCC/UI/Controllers/SCPRProgramDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCPRProgramDetailViewController.h
+//  KPCC
+//
+//  Created by John Meeker on 9/16/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCPRProgramDetailViewController : UIViewController
+
+@end
