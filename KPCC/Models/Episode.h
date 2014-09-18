b8dab59dc8af236c78b0d399f66fbea41d6fd2b5
John Meeker <jmeeker516@gmail.com>
2014-09-18 13:11:13 -0700

    Create Episode model.

---
+++ KPCC/Models/Episode.h
@@ -0,0 +1,13 @@
+//
+//  Episode.h
+//  KPCC
+//
+//  Created by John Meeker on 9/18/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Episode : NSObject
+
+@end
---
+++ KPCC/Models/Episode.m
@@ -0,0 +1,13 @@
+//
+//  Episode.m
+//  KPCC
+//
+//  Created by John Meeker on 9/18/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import "Episode.h"
+
+@implementation Episode
+
+@end
