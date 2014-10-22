1fa0604e1f5b3df8d843a87ce383f7e11fe576e9
John Meeker <jmeeker516@gmail.com>
2014-10-22 12:14:24 -0700

    Create xml parser delegate

---
+++ KPCC/Parsers/SCPRTritonXMLParserDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SCPRTritonXMLParser.h
+//  KPCC
+//
+//  Created by John Meeker on 10/22/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCPRTritonXMLParser : NSXMLParser
+
+@end
---
+++ KPCC/Parsers/SCPRTritonXMLParserDelegate.m
@@ -0,0 +1,13 @@
+//
+//  SCPRTritonXMLParser.m
+//  KPCC
+//
+//  Created by John Meeker on 10/22/14.
+//  Copyright (c) 2014 SCPR. All rights reserved.
+//
+
+#import "SCPRTritonXMLParser.h"
+
+@implementation SCPRTritonXMLParser
+
+@end
