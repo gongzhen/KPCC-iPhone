04ba09e1808d1161ab124448ee791d75bb7b45a4
Christopher Fuller <cfuller@scpr.org>
2016-02-09 14:47:27 -0800

    Remove unused TICK and TOCK macros

--- KPCC/SCPRAppDelegate.h
+++ KPCC/SCPRAppDelegate.h
@@ -11,9 +11,6 @@
 
 #import "SCPRXFSViewController.h"
 
-#define TICK   NSDate *startTime = [NSDate date]
-#define TOCK   NSLog(@"Execution Time: %f  -- [ %s ]=[ Line %d ]", -[startTime timeIntervalSinceNow], __PRETTY_FUNCTION__, __LINE__)
-
 #define kPushChannel @"listenLive"
 #import <Flurry-iOS-SDK/Flurry.h>
 
