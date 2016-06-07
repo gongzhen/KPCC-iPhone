fedcbc316583ac08cbab5207806ddbd96094b37d
Christopher Fuller <cfuller@scpr.org>
2016-06-06 18:25:23 -0700

    Add UserProfileViewController

---
+++ KPCC/UI/Controllers/UserProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserProfileViewController.swift
+//  KPCC
+//
+//  Created by Fuller, Christopher on 6/6/16.
+//  Copyright © 2016 Southern California Public Radio. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UITableViewController {
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
