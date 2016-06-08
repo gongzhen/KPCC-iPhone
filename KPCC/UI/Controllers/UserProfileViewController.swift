ad129b4e8d616e6ac855efb50a844c195549b14f
Christopher Fuller <cfuller@scpr.org>
2016-06-07 18:02:35 -0700

    Add preliminary UserProfileViewController implementation

--- KPCC/UI/Controllers/UserProfileViewController.swift
+++ KPCC/UI/Controllers/UserProfileViewController.swift
@@ -7,15 +7,216 @@
 //
 
 import UIKit
+import Lock
+
+class Value1TableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Value1, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
 
 class UserProfileViewController: UITableViewController {
 
+    lazy var authenticationManager = AuthenticationManager.sharedInstance
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        tableView.registerClass(
+            Value1TableViewCell.self,
+            forCellReuseIdentifier: String(Value1TableViewCell)
+        )
+
+        navigationItem.title = "Profile"
+
+    }
+
+    override func viewWillAppear(animated: Bool) {
+
+        super.viewWillAppear(animated)
+
+        updateUI()
+
+    }
+
+    override func viewDidAppear(animated: Bool) {
+
+        super.viewDidAppear(animated)
+
+        if let userProfile = authenticationManager.userProfile where authenticationManager.isAuthenticated {
+            if !userProfile.isMetaNameEmpty || !userProfile.isMetaPhoneEmpty {
+                promptForNameAndPhone()
+            }
+        }
+
+    }
+
+    func textFieldEditingChanged(sender: AnyObject) {
+        if let alertController = presentedViewController as? UIAlertController {
+            if let defaultAction = alertController.actions.last {
+                let name = alertController.textFields?.first?.text
+                let phone = alertController.textFields?.last?.text
+                let nameCount = (name?.characters.count ?? 0)
+                let phoneCount = (phone?.characters.count ?? 0)
+                let isPhoneNumber = (phone?.isPhoneNumber ?? false)
+                defaultAction.enabled = ((nameCount > 0) && (phoneCount > 0) && isPhoneNumber)
+            }
+        }
+    }
+
+    func logInTapped(sender: AnyObject) {
+        let lockVC = authenticationManager.newLockViewController() {
+            [ weak self ] _ in
+            guard let _self = self else { return }
+            _self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        if let lockVC = lockVC {
+            lockVC.closable = true
+            presentViewController(lockVC, animated: true, completion: nil)
+        }
+    }
+
+    func logOutTapped(sender: AnyObject) {
+        let alertController = UIAlertController(
+            title: nil,
+            message: nil,
+            preferredStyle: .ActionSheet
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "Log Out",
+                style: .Destructive,
+                handler: {
+                    [ weak self ] _ in
+                    guard let _self = self else { return }
+                    _self.authenticationManager.reset()
+                    _self.updateUI()
+                }
+            )
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "Cancel",
+                style: .Cancel,
+                handler: nil
+            )
+        )
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 3
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(String(Value1TableViewCell))!
+        if let userProfile = authenticationManager.userProfile {
+            switch indexPath.row {
+            case 0:
+                cell.textLabel?.text = "Email"
+                cell.detailTextLabel?.text = userProfile.email
+            case 1:
+                cell.textLabel?.text = "Name"
+                cell.detailTextLabel?.text = userProfile.metaName
+            case 2:
+                cell.textLabel?.text = "Phone"
+                cell.detailTextLabel?.text = userProfile.metaPhone
+            default:
+                cell.textLabel?.text = nil
+                cell.detailTextLabel?.text = nil
+            }
+        }
+        else {
+            cell.textLabel?.text = nil
+            cell.detailTextLabel?.text = nil
+        }
+        return cell
+    }
+
+    private func updateUI() {
+        if authenticationManager.isAuthenticated {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: "Log Out",
+                style: .Plain,
+                target: self,
+                action: #selector(logOutTapped)
+            )
+        }
+        else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                title: "Log In",
+                style: .Plain,
+                target: self,
+                action: #selector(logInTapped)
+            )
+        }
+        tableView.reloadData()
+    }
+
+    private func promptForNameAndPhone() {
+        let alertController = UIAlertController(
+            title: "One More Thing",
+            message: "If you ever win a contest or drawing, we'll need to contact you quickly.",
+            preferredStyle: .Alert
+        )
+        alertController.addTextFieldWithConfigurationHandler {
+            [ weak self ] textField in
+            guard let _self = self else { return }
+            textField.placeholder = "First and Last Name"
+            textField.text = _self.authenticationManager.userProfile?.metaName
+            textField.addTarget(
+                self,
+                action: #selector(_self.textFieldEditingChanged),
+                forControlEvents: .EditingChanged
+            )
+        }
+        alertController.addTextFieldWithConfigurationHandler {
+            [ weak self ] textField in
+            guard let _self = self else { return }
+            textField.placeholder = "Phone Number"
+            textField.text = _self.authenticationManager.userProfile?.metaPhone
+            textField.addTarget(
+                self,
+                action: #selector(_self.textFieldEditingChanged),
+                forControlEvents: .EditingChanged
+            )
+        }
+        alertController.addAction(
+            UIAlertAction(
+                title: "Cancel",
+                style: .Cancel,
+                handler: nil
+            )
+        )
+        let defaultAction = UIAlertAction(
+            title: "Submit",
+            style: .Default,
+            handler: {
+                [ weak self ] _ in
+                guard let _self = self else { return }
+                let name = alertController.textFields?.first?.text
+                let phone = alertController.textFields?.last?.text
+                _self.authenticationManager.updateUser(name: name, phone: phone) {
+                    [ weak self ] _ in
+                    guard let _self = self else { return }
+                    _self.updateUI()
+                }
+            }
+        )
+        defaultAction.enabled = false
+        alertController.addAction(defaultAction)
+        presentViewController(alertController, animated: true, completion: nil)
     }
 
 }
