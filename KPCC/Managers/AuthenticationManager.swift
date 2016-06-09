3ab850dbaa0321aab3526d895cd6ac080800ff12
Christopher Fuller <cfuller@scpr.org>
2016-06-08 18:27:42 -0700

    Add KPCCTheme method to A0Theme extension

--- KPCC/Managers/AuthenticationManager.swift
+++ KPCC/Managers/AuthenticationManager.swift
@@ -81,6 +81,130 @@ extension A0SimpleKeychain {
 
 }
 
+extension A0Theme {
+
+    static func KPCCTheme(bundle bundle: NSBundle) -> A0Theme {
+
+        let theme = A0Theme()
+
+        theme.statusBarStyle = .Default
+
+        theme.registerColor(
+            UIColor(r: 49, g: 171, b: 212),
+            forKey: A0ThemePrimaryButtonNormalColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 0, g: 115, b: 151),
+            forKey: A0ThemePrimaryButtonHighlightedColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Semibold.name, size: 18.0) {
+            theme.registerFont(font, forKey: A0ThemePrimaryButtonFont)
+        }
+
+        theme.registerColor(
+            UIColor.whiteColor(),
+            forKey: A0ThemePrimaryButtonTextColor
+        )
+
+        theme.registerColor(
+            UIColor.clearColor(),
+            forKey: A0ThemeSecondaryButtonBackgroundColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Book.name, size: 16.0) {
+            theme.registerFont(font, forKey: A0ThemeSecondaryButtonFont)
+        }
+
+        theme.registerColor(
+            UIColor(r: 49, g: 171, b: 212),
+            forKey: A0ThemeSecondaryButtonTextColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Book.name, size: 16.0) {
+            theme.registerFont(font, forKey: A0ThemeTextFieldFont)
+        }
+
+        theme.registerColor(
+            UIColor(r: 133, g: 133, b: 133),
+            forKey: A0ThemeTextFieldTextColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 155, g: 165, b: 169),
+            forKey: A0ThemeTextFieldPlaceholderTextColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 49, g: 171, b: 212),
+            forKey: A0ThemeTextFieldIconColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Book.name, size: 27.0) {
+            theme.registerFont(font, forKey: A0ThemeTitleFont)
+        }
+
+        theme.registerColor(
+            UIColor(r: 248, g: 126, b: 33),
+            forKey: A0ThemeTitleTextColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Book.name, size: 17.0) {
+            theme.registerFont(font, forKey: A0ThemeDescriptionFont)
+        }
+
+        theme.registerColor(
+            UIColor(r: 108, g: 117, b: 121),
+            forKey: A0ThemeDescriptionTextColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 242, g: 242, b: 242),
+            forKey: A0ThemeScreenBackgroundColor
+        )
+
+        theme.registerImageWithName(
+            "KPCCLogo30",
+            bundle: bundle,
+            forKey: A0ThemeIconImageName
+        )
+
+        theme.registerColor(
+            UIColor(r: 247, g: 247, b: 247),
+            forKey: A0ThemeIconBackgroundColor
+        )
+
+        if let font = UIFont(name: FreightSansPro.Medium.name, size: 14.0) {
+            theme.registerFont(font, forKey: A0ThemeSeparatorTextFont)
+        }
+
+        theme.registerColor(
+            UIColor(r: 108, g: 117, b: 121),
+            forKey: A0ThemeSeparatorTextColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 201, g: 213, b: 216),
+            forKey: A0ThemeCredentialBoxBorderColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 201, g: 213, b: 216),
+            forKey: A0ThemeCredentialBoxSeparatorColor
+        )
+
+        theme.registerColor(
+            UIColor(r: 242, g: 242, b: 242),
+            forKey: A0ThemeCredentialBoxBackgroundColor
+        )
+
+        return theme
+
+    }
+
+}
+
 extension A0UserProfile {
 
     var metaName: String? {
