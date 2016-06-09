9afa3a13b25b5906dbcdd10c5df2dfd5d6db35bd
Christopher Fuller <cfuller@scpr.org>
2016-06-09 11:03:45 -0700

    Add PreRollAdController

---
+++ KPCC/Library/PreRollAdController.swift
@@ -0,0 +1,15 @@
+//
+//  PreRollAdController.swift
+//  KPCC
+//
+//  Created by Fuller, Christopher on 6/9/16.
+//  Copyright © 2016 Southern California Public Radio. All rights reserved.
+//
+
+import UIKit
+
+@objc class PreRollAdController: NSObject {
+
+    weak var viewController: UIViewController?
+
+}
