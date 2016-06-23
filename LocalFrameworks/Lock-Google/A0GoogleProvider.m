f6e0c7bb84744bda54567d3e4bb2d874ac061cab
Christopher Fuller <cfuller@scpr.org>
2016-06-23 14:30:56 -0700

    Fix compile error in A0GoogleProvider

--- LocalFrameworks/Lock-Google/A0GoogleProvider.m
+++ LocalFrameworks/Lock-Google/A0GoogleProvider.m
@@ -57,7 +57,7 @@
         authentication.scopes = scopes;
         authentication.delegate = self;
         authentication.uiDelegate = self;
-        authentication.allowsSignInWithWebView = YES;
+//        authentication.allowsSignInWithWebView = YES;
         _authentication = authentication;
         _onAuthentication = ^(NSError *error, NSString *token) {};
         _authenticating = NO;
